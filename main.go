2e39ef7afbc306cfe97012715037a1fee613b218
khoale <khoa_le@onskyinc.com>
2020-01-20 12:18:02 +0700

    add func ErrorMessageTranslater

--- main.go
+++ main.go
@@ -5,16 +5,44 @@ package errors
 import (
 	"encoding/json"
 	"fmt"
+	yaml "gopkg.in/yaml.v2"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
-// RedisEmpty redis empty value response
-const RedisEmpty = "redis: nil"
+
+const (
+	// RedisEmpty redis empty value response
+	RedisEmpty = "redis: nil"
+	// NotSupport means language not support yet
+	NotSupport = "language:notSupport"
+	// NotExisted means message type not existed
+	NotExisted = "messageType:notExisted"
+)
+
 // Error implements the error interface.
 type Error struct {
 	Id     string `json:"id"`
 	Code   int32  `json:"code"`
 	Detail string `json:"detail"`
 	Status string `json:"status"`
+}
+
+// ErrorDict represents error list
+type ErrorDict struct {
+	ErrorList []ErrorMessage `yaml:"error_list,omitempty" json:"errorList,omitempty"`
+}
+
+// ErrorMessage holds message type for many languages
+type ErrorMessage struct {
+	Type              string    `yaml:"type,omitempty" json:"type,omitempty"`
+	TranslatedMessage []Message `yaml:"translated_message,omitempty" json:"translated_message,omitempty"`
+}
+
+// Message represents message for a language
+type Message struct {
+	Text     string `yaml:"text,omitempty" json:"text,omitempty"`
+	Language string `yaml:"language,omitempty" json:"language,omitempty"`
 }
 
 func (e *Error) Error() string {
@@ -121,4 +149,48 @@ func InternalServerError(id, format string, a ...interface{}) error {
 		Detail: fmt.Sprintf(format, a...),
 		Status: http.StatusText(500),
 	}
+}
+
+// ErrorMessageTranslater converts messageType to message for input language
+func ErrorMessageTranslater(path string, messageType string, language string) (string, error) {
+	var transMsg string
+	errorDict, err := LoadErrorList(path)
+	if err != nil {
+		return "", err
+	}
+	if errorDict.ErrorList != nil && len(errorDict.ErrorList) > 0 {
+		for _, v := range errorDict.ErrorList {
+			if v.Type == messageType {
+				for _, t := range v.TranslatedMessage {
+					if t.Language == language {
+						transMsg = t.Text
+						return transMsg, nil
+					}
+				}
+				return NotSupport, nil
+			}
+		}
+		return NotExisted, nil
+	}
+
+	return "", nil
+}
+
+// LoadErrorList loads error list from file
+func LoadErrorList(path string) (*ErrorDict, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("error reading error list file, %s", err)
+	}
+	var cfg = new(ErrorDict)
+	if err := yaml.Unmarshal(bytes, cfg); err != nil {
+		return nil, fmt.Errorf("unable to decode into struct, %v", err)
+	}
+	return cfg, nil
 }
